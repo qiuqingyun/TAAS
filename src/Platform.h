b5bb2991d49670c4d6b3cb4a918c4b831ad1f092
qqy <809969480@qq.com>
2023-05-10 05:56:47 +0000

    p

--- src/Platform.h
+++ src/Platform.h
@@ -535,7 +535,6 @@ public:
             BN_free(c[j]);
         }
         delete[] c;
-        BN_free(tq);
         BN_free(kq);
         BN_free(ta);
         BN_CTX_end(ctx);
@@ -737,50 +736,6 @@ public:
         BIGNUM *k2_ = BN_rand(256);
         BIGNUM *kq_ = BN_rand(256);
         // 选择m个随机数 {b1,b2,...,bm}
-        BIGNUM **b = new BIGNUM *[user_count_platform];
-        // 选择n个随机数{c1,c2,...,cn}
-        BIGNUM **c = new BIGNUM *[user_count_advertiser];
-        // 设置 Q'=0
-        message_p3_->Q_ = EC_POINT_new(w1->get_curve());
-        // 保存向量J
-        message_p3_->J = new EC_POINT *[user_count_platform];
-// 并行化
-#pragma omp parallel for
-        for (int j = 0; j < user_count_platform; ++j)
-        {
-            BN_CTX *temp_ctx = BN_CTX_new();
-            b[j] = BN_rand(256);
-            // 计算 Jj = k2*Qj
-            message_p3_->J[j] = EC_POINT_new(w1->get_curve());
-            EC_POINT_mul(w1->get_curve(), message_p3_->J[j], NULL, message_a2->Q[j], k2, temp_ctx);
-            // 计算 Q' = Q' + bj*Qj
-            EC_POINT *temp = EC_POINT_new(w1->get_curve());
-            EC_POINT_mul(w1->get_curve(), temp, NULL, message_a2->Q[j], b[j], temp_ctx);
-// 线程安全
-#pragma omp critical
-            // 累加 Q'
-            EC_POINT_add(w1->get_curve(), message_p3_->Q_, message_p3_->Q_, temp, temp_ctx);
-            // 释放内存
-            EC_POINT_free(temp);
-            BN_CTX_free(temp_ctx);
-        }
-        // 计算 C2 = k2*Q'
-        message_p3_->C2 = EC_POINT_new(w1->get_curve());
-        EC_POINT_mul(w1->get_curve(), message_p3_->C2, NULL, message_p3_->Q_, k2, ctx);
-        // 计算 C2' = k2'*Q'
-        message_p3_->C2_ = EC_POINT_new(w1->get_curve());
-        EC_POINT_mul(w1->get_curve(), message_p3_->C2_, NULL, message_p3_->Q_, k2_, ctx);
-        // 计算哈希值 tq = H(W_1||C2')
-        BIGNUM *tq = BN_hash(
-            w1->to_string(ctx),
-            EC_POINT_to_string(w1->get_curve(), message_p3_->C2_, ctx));
-        // 计算 k2_hat = tq*k2 + k2'
-        message_p3_->k2_hat = BN_new();
-        BN_mod_mul(message_p3_->k2_hat, tq, k2, w1->get_order(), ctx);
-        BN_mod_add(message_p3_->k2_hat, message_p3_->k2_hat, k2_, w1->get_order(), ctx);
-        BN_free(tq);
-
-
 
         /*开始与round_P3不同*/
 
@@ -1109,11 +1064,6 @@ public:
         BN_free(k2_);
         BN_free(kq_);
         BN_free(tp_h);
-        for (int j = 0; j < user_count_platform; ++j)
-        {
-            BN_free(b[j]);
-        }
-        delete[] b;
         
         for (int i = 0; i < user_count_advertiser; ++i)
         {
